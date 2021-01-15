d788f7275a69b69bd44845c8b4c99b26f0a9e933
Prabal Singh <praba170107045@iitg.ac.in>
2021-01-15 11:39:02 +0530

    chore: add feature to create elasticsearch index

--- helpers/search.js
+++ helpers/search.js
@@ -1,12 +1,13 @@
 const elasticsearch = require('elasticsearch');
+const cities = require('../data/cities');
 
-let index = 'autosuggest-index';
+let index = 'cities-index';
 let client = null;
+const maxBatchSize = 200;
 
 
 async function init(elasticsearchConfig){
 	client = new elasticsearch.Client(elasticsearchConfig);
-
 	try {
 		const indexAlreadyExists = await client.indices.exists({index});
 		if (indexAlreadyExists) {
@@ -22,7 +23,65 @@ async function init(elasticsearchConfig){
 
 
 async function generateIndex() {
-	console.log('regerating index');
+
+	// first, we will delete the index, if it exists
+	const indexAlreadyExists = await client.indices.exists({index});
+
+	if (indexAlreadyExists) {
+		await client.indices.delete({index});
+	}
+
+	const indexMapping = {
+		"mappings": {
+		     "city": { 
+		       "properties": { 
+			         "cityName":     { "type": "keyword"  },
+		        }
+		     }
+		}
+	}	
+
+	await client.indices.create(
+		{body: indexMapping, index}
+	);
+
+	
+	const bulkBatches = [];
+
+	let idx = 0;
+	while(idx < cities.length){
+		const currentBatch = [];
+		while(idx < cities.length && currentBatch.length < maxBatchSize){
+			currentBatch.push({cityName: cities[idx].name});
+			idx ++;
+		}
+		bulkBatches.push(currentBatch);
+	}
+
+	bulkBatches.forEach(batch => processCurrentBatch(batch));
+
+	await refreshIndex();
+}
+
+async function processCurrentBatch(batch){
+	const bulkOperations = batch.reduce((accumulator, city) => {
+		accumulator.push({
+			index: {
+				_index: index,
+				_type: "city"
+			}
+		});
+		accumulator.push(city);
+		return accumulator;
+	}, []);
+	
+	await client.bulk({
+		body: bulkOperations
+	});	
+}
+
+function refreshIndex() {
+	return client.indices.refresh({index});
 }
 
 module.exports = {
