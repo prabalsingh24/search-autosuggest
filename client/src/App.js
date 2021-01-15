97cc6bbd1f4fe8219a64b21d8786fbf04c45afb9
Prabal Singh <praba170107045@iitg.ac.in>
2021-01-15 17:51:50 +0530

    chore: create a simple search bar and connect it with node

---
+++ client/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import AsyncSelect from 'react-select/async';
+import './App.css';
+
+
+class App extends React.Component {
+  	constructor(props){
+  		super(props);
+  		this.state = {
+  			selectedOption: null
+  		}
+  		this.fetchOptions = this.fetchOptions.bind(this);
+  		this.handleChange = this.handleChange.bind(this);
+  	}
+
+
+
+	async fetchOptions(query) {
+		const newOptions = await fetch(`http://localhost:8000/search/autocomplete?q=${query}`)
+		.then(res => res.text())
+		.then(res => JSON.parse(res))
+		.then(res => res.cityNames);
+		const formattedOptions = newOptions.map(option => {
+			return {
+				value: option, 
+				label: option
+			}
+		});
+		return formattedOptions;
+	}
+
+
+	async handleChange(query){
+		this.setState({selectedOption: query});
+	}
+
+  	render(){
+  		const { selectedOption } = this.state;
+
+  	  	return (
+  	  		     <AsyncSelect
+		         	className="search"
+ 			        value={selectedOption}
+			        onInputChange={this.handleChange}
+			        onChange={this.handleChange}
+			        loadOptions={this.fetchOptions}
+			      />
+  		);
+  	}
+  	
+}
+  
+
+export default App;
