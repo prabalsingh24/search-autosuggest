aa520583a29a8c5e71300d54b81963a142e96ce7
Prabal Singh <praba170107045@iitg.ac.in>
2021-01-15 11:41:31 +0530

    chore: use cities.json as dataset for elasticsearch entries

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express();
 const port = 8000;
+const routes = require('./routes');
+const search = require('./helpers/search')
+const config = require('./config/config')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+
+// setup the routes
+app.use(routes);
+
+// setup elasticsearch
+search.init(config.search);
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+  console.log(`Listening on port ${port}!`)
 });
